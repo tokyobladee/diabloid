69aa488579fd4478d5c8de3ec07c8a7550a89717
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2023-04-29 22:11:59 +0300

    init

---
+++ Assets/scripts/StartMessage.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+public class StartMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Game starts.");
+    }
+}
