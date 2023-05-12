3271e97c1a34a33be4ef788f68bbd131158b193c
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2023-05-12 14:08:38 +0300

    2.4.3
    Collider

--- Assets/scripts/Movement.cs
+++ Assets/scripts/Movement.cs
@@ -36,5 +36,24 @@ public class Movement : MonoBehaviour
         
         transform.Translate(moveDelta * Time.deltaTime * speed);
         
+        //collider
+
+        hit = Physics2D.BoxCast(transform.position, collider.size, 0, new Vector2(0, moveDelta.y),
+            Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            transform.Translate(0,moveDelta.y * Time.deltaTime, 0);
+        }
+        
+        
+        hit = Physics2D.BoxCast(transform.position, collider.size, 0, new Vector2(moveDelta.x, 0),
+            Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+
+        if (hit.collider == null)
+        {
+            transform.Translate(moveDelta.x * Time.deltaTime,0, 0);
+        }
+        
     }
 }
